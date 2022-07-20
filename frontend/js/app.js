0e5ce0f3f114075a7260240300f9ecb0bfacdd62
Devesh Sharma <allahsown6000@gmail.com>
2022-07-20 19:12:55 +0530

    javascript app.js faq update

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -10,6 +10,23 @@ window.addEventListener("DOMContentLoaded", async () => {
   welcomeH2.innerText = welcome_h2;
   welcomeP.innerHTML = welcome_p;
 
+var faq = document.getElementsByClassName("faq-page");
+var i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
+
   if (window.ethereum) {
     window.web3 = new Web3(window.ethereum);
     checkChain();
